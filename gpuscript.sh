6674b79cf2d96c7dd690283071b6259cbc23975b
Tudor Ionel <tropiko.matrox@gmail.com>
2016-07-29 23:44:21 +0300

    Add files via upload

---
+++ gpuscript.sh
@@ -0,0 +1,18 @@
+# This script prints your nvidia GPU temperature if
+# you have installed video-nvidia drivers
+
+# Press [q] to quit
+
+#!/bin/bash
+while true; do
+	TEMP=$(nvidia-smi -a | grep "GPU Current Temp" | grep -o '[0-9]\+')
+	
+	printf "GTX 770 Temp: %s\r" "$TEMP"
+    	read -t 1 -n 1 key
+
+    	if [[ $key = q ]] #|| [[ $key = $escape ]]
+    	then
+        	break
+    	fi
+	sleep 1
+done
